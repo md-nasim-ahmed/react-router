87b3d694242c321f5f1ed68fb43c9c05e4e1af2c
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-03-02 05:21:28 -0500

    html to image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import cogoToast from 'cogo-toast';
+import * as htmlToImage from 'html-to-image';
+// import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 import React from 'react';
 
-// import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 const App = () => {
 
-  const ErrorToast=()=>{
-      cogoToast.error('Error')
-  }
-    const SuccessToast=()=>{
-      cogoToast.success('success')
+    const DownloadImg = () => {
+        let node = document.getElementById('tableList')
+        htmlToImage.toPng(node).then((dataUrl) => {
+            let img = new Image();
+            img.src = dataUrl;
+            document.body.appendChild(img);
+        }).catch((error) => {
+            console.log(error)
+        })
     }
-  return (
-    <div> 
-      <button onClick={ErrorToast} className='btn btn-danger'>Error</button>
-      <button onClick={SuccessToast} className='btn btn-success'>Success</button>
-    </div>
-  );
+
+
+    return (
+        <div>
+            <div id="tableList" className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <table>
+                            <tr>
+                                <td>Nasim</td>
+                                <td>Dhaka</td>
+                            </tr>
+                            <tr>
+                                <td>BULBUL</td>
+                                <td>Dhaka</td>
+                            </tr>
+                            <tr>
+                                <td>Nasim</td>
+                                <td>Dhaka</td>
+                            </tr>
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <button onClick={DownloadImg} className='btn btn-danger m-5'>Download Image</button>
+        </div>
+    );
 };
 
 export default App;
-
