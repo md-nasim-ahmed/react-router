d72c62d6d79c0279afa4fff119c10ff70e9d1974
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-03-04 14:56:41 -0500

    react code input

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import readXlsxFile from 'read-excel-file'
+import ReactCodeInput from 'react-code-input';
+
 
 const App = () => {
-    const Readfile = () => {
-        let myfile = document.getElementById("myfile")
-
-        readXlsxFile(myfile.files[0]).then((data) => {
-            alert(data)
-        })
-
-    }
 
     return (
         <div>
-            <input id="myfile" className="m-2 form-control" type="file"></input>
-            <button onClick={Readfile} className="btn m-2 btn-danger">Read excel</button>
+            <ReactCodeInput onChange={(value) => { alert(value) }} type='number' fields={6} /><br />
+
         </div>
     );
 };
